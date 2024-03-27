8491049de42f2b5320a988f3c141286eec900472
PSharp725 <PatrickSharp725@gmail.com>
2024-03-27 00:53:59 -0600

    Fixed an error in the SQL statement

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_world():
     return 'Hello World from Patrick in 3308'
 
 @app.route('/db_test')
-def db_test():
+def testing():
     conn = psycopg2.connect("postgres://psharp725_hello_world_user:mki9WoZr1id3v32XMjXRxuFsgI9ycc1j@dpg-co1rlv821fec73d3uqv0-a/psharp725_hello_world")
     conn.close()
     return "Database Connection Successful"
@@ -21,7 +21,7 @@ def creating():
     First varchar(255),
     Last varchar(255),
     City varchar(255),
-    Name carchar(255),
+    Name varchar(255),
     Number int
     );
     ''')
