82c040c291620cc9c711a445f091b685a650d002
PSharp725 <PatrickSharp725@gmail.com>
2024-03-27 01:15:30 -0600

    Added db_select route

--- app.py
+++ app.py
@@ -44,3 +44,23 @@ def inserting():
     conn.commit()
     conn.close()
     return 'Basketball Table Successfully Populated'
+
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect('postgres://psharp725_hello_world_user:mki9WoZr1id3v32XMjXRxuFsgI9ycc1j@dpg-co1rlv821fec73d3uqv0-a/psharp725_hello_world')
+    cur = conn.cursor()
+    cur.execute('''
+    SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ''
+    response_string += '<table>'
+    for player in records:
+        response_string += '<tr>'
+        for info in player:
+            response_string += '<td>{}</td>'.format(info)
+        response_string += '</tr>'
+    response_string += '</table>'
+    return response_string
